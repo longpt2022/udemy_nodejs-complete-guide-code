dca56dae8e54d5bde66c2f615ba2fbdaa493fe16
longpt271 <longpt271@gmail.com>
2022-12-13 20:44:42 +0700

    30. Sending Responses

--- B1/26 - 27. Creating a Node Server/app.js
+++ B1/26 - 27. Creating a Node Server/app.js
@@ -7,6 +7,16 @@ const server = http.createServer((req, res) => {
 
   // Lệnh thoát khỏi event loop của server
   // process.exit();
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js</h1></body>');
+  res.write('</head>');
+  res.end();
+
+  // k viết write() sau end() -> sẽ lỗi
+  // res.write;
 });
 
 //  lắng nghe port
