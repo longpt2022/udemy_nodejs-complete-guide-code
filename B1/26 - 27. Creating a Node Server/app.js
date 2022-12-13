eaf5b87e5e9713dcb5c0811af1893d659e5339b9
longpt271 <longpt271@gmail.com>
2022-12-13 20:53:37 +0700

    32. Routing Requests

--- B1/26 - 27. Creating a Node Server/app.js
+++ B1/26 - 27. Creating a Node Server/app.js
@@ -3,10 +3,17 @@ const http = require('http');
 // --- tạo server
 // req: request, res: response
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-
-  // Lệnh thoát khỏi event loop của server
-  // process.exit();
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" /><button type="submit">Send</button></form></body>'
+    );
+    res.write('</head>');
+    // thêm "return" để những đoạn code "res" ở dưới end() k đc thực hiện
+    return res.end();
+  }
 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
@@ -14,9 +21,6 @@ const server = http.createServer((req, res) => {
   res.write('<body><h1>Hello from my Node.js</h1></body>');
   res.write('</head>');
   res.end();
-
-  // k viết write() sau end() -> sẽ lỗi
-  // res.write;
 });
 
 //  lắng nghe port
