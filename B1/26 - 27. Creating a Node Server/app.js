d8f7fdd491c873a2678c0c4d11c2c7e5609cb864
longpt271 <longpt271@gmail.com>
2022-12-13 21:45:15 +0700

    34. Parsing Request Bodies

--- B1/26 - 27. Creating a Node Server/app.js
+++ B1/26 - 27. Creating a Node Server/app.js
@@ -10,15 +10,25 @@ const server = http.createServer((req, res) => {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" /><button type="submit">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>'
     );
-    res.write('</head>');
+    res.write('</html>');
     // thêm "return" để những đoạn code "res" ở dưới end() k đc thực hiện
     return res.end();
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
@@ -26,8 +36,8 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js</h1></body>');
-  res.write('</head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
   res.end();
 });
 
