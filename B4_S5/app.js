130847d47075b9c33b94a7f5e32ae84ef0a959b8
longpt271 <longpt271@gmail.com>
2022-12-14 20:57:36 +0700

    60. Adding Middleware

--- B4_S5/app.js
+++ B4_S5/app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+// tạo middleware thông qua use()
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next(); // Cho phép request tới middleware dòng code tiếp theo
+});
+
+app.use((req, res, next) => {
+  console.log('In the another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
