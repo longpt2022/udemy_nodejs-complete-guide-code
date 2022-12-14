023d30d65eb40eb6abaccc97eefeb9e8e33dd355
longpt271 <longpt271@gmail.com>
2022-12-14 21:16:12 +0700

    63. Handling Different Routes

--- B4_S5/app.js
+++ B4_S5/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
-// tạo middleware thông qua use()
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // Cho phép request tới middleware dòng code tiếp theo
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In the another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('In the another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
