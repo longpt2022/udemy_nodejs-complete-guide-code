b2d98035820df43ea7cabc55adddf876963387ac
longpt271 <longpt271@gmail.com>
2022-12-15 16:58:55 +0700

    70. Serving HTML Pages

--- B4_S5/routes/shop.js
+++ B4_S5/routes/shop.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+  // __dirname: dẫn tới "toàn bộ thư mục" trong code
+  // views: dẫn đến thư mục "views"
+  // shop.html: dẫn đến file "shop.html"
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
