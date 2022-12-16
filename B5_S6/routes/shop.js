d3616016fdf082b4c82fa7a600e6316d37f652c3
longpt271 <longpt271@gmail.com>
2022-12-16 22:50:37 +0700

    81. Installing & Implementing Pug

--- B5_S6/routes/shop.js
+++ B5_S6/routes/shop.js
@@ -8,11 +8,7 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  // __dirname: dẫn tới "toàn bộ thư mục" trong code
-  // views: dẫn đến thư mục "views"
-  // shop.html: dẫn đến file "shop.html"
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
