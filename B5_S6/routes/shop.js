f71b8f65c71dfc239c9f452cd17478463e09aaf8
longpt271 <longpt271@gmail.com>
2022-12-16 22:29:19 +0700

    79. Sharing Data Across Requests & Users

--- B5_S6/routes/shop.js
+++ B5_S6/routes/shop.js
@@ -3,10 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products);
   // __dirname: dẫn tới "toàn bộ thư mục" trong code
   // views: dẫn đến thư mục "views"
   // shop.html: dẫn đến file "shop.html"
