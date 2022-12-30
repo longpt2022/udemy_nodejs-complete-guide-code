c81840cca1f76fbfbb268d250bdefb70adb5b208
longpt271 <longpt271@gmail.com>
2022-12-30 23:49:50 +0700

    151. Connecting to the Database

--- B10_S11/util/database.js
+++ B10_S11/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'abc@123', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'abc@123',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
