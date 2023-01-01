f73c4ea4855100208b3363ebee9737d5e30bd3b0
longpt271 <longpt271@gmail.com>
2023-01-01 21:30:37 +0700

    182. Finishing the Database Connection

--- B12_S12/util/database.js
+++ B12_S12/util/database.js
@@ -1,15 +1,30 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+// "_" có nghĩa rằng điều này chỉ được sử dụng nội bộ trong tệp này
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://longpham:YourPassword@cluster0.qdrrmtw.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://longpham:YourPassword@cluster0.qdrrmtw.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
