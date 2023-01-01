98ac68a54397849c5527a16ad4e6488f9d3d47c7
longpt271 <longpt271@gmail.com>
2023-01-01 19:49:22 +0700

    180. Installing the MongoDB Driver

--- B12_S12/util/database.js
+++ B12_S12/util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'abc@123', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://longpham:YourPassword@cluster0.qdrrmtw.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
