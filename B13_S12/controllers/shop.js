f9e945a94cbb426673930337f99a88873d876e82
longpt271 <longpt271@gmail.com>
2023-01-02 18:03:31 +0700

    200. Fixing a Bug

--- B13_S12/controllers/shop.js
+++ B13_S12/controllers/shop.js
@@ -71,8 +71,8 @@ exports.postCart = (req, res, next) => {
     })
     .then(result => {
       console.log(result);
-    })
-    .catch(err => console.log(err));
+      res.redirect('/cart');
+    });
   // let fetchedCart;
   // let newQuantity = 1;
   // req.user
