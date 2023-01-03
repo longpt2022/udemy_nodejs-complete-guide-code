5c320014cb834beefab5efc40d9e962c76324cf7
longpt271 <longpt271@gmail.com>
2023-01-03 22:52:07 +0700

    Start bai 14 - section 13

---
+++ B14_S13/app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  // Nếu có ít nhất 1 user. findById() nhận "1" có nghĩa là nhận vào "true"
+  //  Tạo user mới trên mongoDB compass để lấy được Id truyền vào findById()
+  User.findById('63b2973cb766ab62a39abb0e')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
