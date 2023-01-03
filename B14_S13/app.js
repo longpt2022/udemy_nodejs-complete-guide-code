c4a77e063c83f3d624863644d034c1dc3d43ab4f
longpt271 <longpt271@gmail.com>
2023-01-04 01:23:05 +0700

    218. Adding and Using a User Model

--- B14_S13/app.js
+++ B14_S13/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,16 +18,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   // Nếu có ít nhất 1 user. findById() nhận "1" có nghĩa là nhận vào "true"
-//   //  Tạo user mới trên mongoDB compass để lấy được Id truyền vào findById()
-//   User.findById('63b2973cb766ab62a39abb0e')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  // lấy được Id sau khi tạo user
+  User.findById('63b4726355f7286690fd1798')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -39,6 +38,17 @@ mongoose
     'mongodb+srv://longpham:YourPassword@cluster0.qdrrmtw.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        //  Tạo user mới trên mongoDB compass
+        const user = new User({
+          name: 'Long',
+          email: 'long@test.com',
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
